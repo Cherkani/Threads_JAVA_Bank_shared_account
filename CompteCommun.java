1252e0b2eeff4f7d71727075310573bbcfde74d1
Cherkani Aymen <124716884+Cherkani@users.noreply.github.com>
2024-03-13 01:08:01 +0000

    Update CompteCommun.java

--- CompteCommun.java
+++ CompteCommun.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Threads_Bank_shared_account;
 
 import java.util.Random;
